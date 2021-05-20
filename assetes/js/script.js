35a8aafba3bf5bdf6a0de4f3922871e9c4e6060a
Candra Kartika <candrakartika.com>
2021-05-20 11:16:33 +0700

    add style css to form

--- assetes/js/script.js
+++ assetes/js/script.js
@@ -7,20 +7,23 @@ form.addEventListener('submit', (e) => {
 
     const gender = document.querySelector('input[type="radio"]:checked').value;
     const tinggi = document.getElementById("tinggi").value;
-    const result = document.querySelector("h4");
+    const result = document.getElementById("hasil");
+
+    let hasil = "";
   
+  
+        if (gender === "woman") {
+            beratIdeal = (tinggi - 100) - (tinggi - 100) * 0.15
 
-    if (gender === "cewe") {
-        beratIdeal = (tinggi - 100) - (tinggi - 100) * 0.15
+        } else {
 
-    } else {
+            beratIdeal = (tinggi - 100) - (tinggi - 100) * 0.10
 
-        beratIdeal = (tinggi - 100) - (tinggi - 100) * 0.10
+        }
 
-    }
-
-
-    const hasil = 'berat badan kamu: ' + beratIdeal + ' kg';
+         hasil = beratIdeal + ' kg';
+   
+   
     result.innerText = hasil;
     e.preventDefault();
 
